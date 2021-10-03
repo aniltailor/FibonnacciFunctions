1eef989fa395f30beb47e2e737e122f43dbbce76
Anil Tailor <anil.tailor@mandg.com>
2021-10-03 18:12:51 +0100

    Removed redundant tests

--- source/FindInFibonaccis.Tests/FindInFibonacciShould.cs
+++ source/FindInFibonaccis.Tests/FindInFibonacciShould.cs
@@ -22,46 +22,5 @@ namespace FindInFibonacci.Tests
             var result = _findInFibonacciService.FindFibonacciNumbers(integerForSearch);
             result.Should().Be(expectedResult, "Should find nearest Fibonacci number and its closest neighbours");
         }
-
-        [Test]
-        public void ReturnExpectedFibonacciNumberGivenAnIntegerOf6()
-        {
-            const int integerForSearch = 6;
-            const string expectedResult = "5 3 8";
-            var result = _findInFibonacciService.FindFibonacciNumbers(integerForSearch);
-
-            result.Should().Be(expectedResult,"Should find nearest Fibonacci number and its closest neighbours");
-        }
-
-        [Test]
-        public void ReturnZeroGivenAnIntegerOf0()
-        {
-            const int integerForSearch = 0;
-            const string expectedResult = "0";
-            var result = _findInFibonacciService.FindFibonacciNumbers(integerForSearch);
-
-            result.Should().Be(expectedResult, "Should find nearest Fibonacci number and its closest neighbours");
-        }
-
-        [Test]
-        public void ReturnExpectedFibonacciNumberGivenAnIntegerOf20()
-        {
-            const int integerForSearch = 20;
-            const string expectedResult = "21 13 34";
-            var result = _findInFibonacciService.FindFibonacciNumbers(integerForSearch);
-
-            result.Should().Be(expectedResult, "Should find nearest Fibonacci number and its closest neighbours");
-        }
-
-        [Test]
-        public void ReturnExpectedFibonacciNumberGivenAnIntegerOf88()
-        {
-            const int integerForSearch = 88;
-            const string expectedResult = "89 55 144";
-            var result = _findInFibonacciService.FindFibonacciNumbers(integerForSearch);
-
-            result.Should().Be(expectedResult, "Should find nearest Fibonacci number and its closest neighbours");
-        }
-
     }
 }
